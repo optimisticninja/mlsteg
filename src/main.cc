1e677a2e7a7a5ceed12b0224e049e202a28fbe7a
John Holly <j@optimistic.ninja>
2021-08-29 17:56:47 -0700

    bpnn.cc -> main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,116 +1,64 @@
-#include <fstream>
 #include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <numeric>
 #include <random>
-#include <regex>
-#include <string>
-#include <sys/stat.h>
 #include <vector>
-
-#include <zlib.h>
+#include <map>
 
 #include <boost/program_options.hpp>
 
-#include "compression.h"
-#include "crypto/kdf/fastpbkdf2.h"
-#include "crypto/mode/aead.h"
-#include "crypto/mode/ctr.h"
-#include "dna64.h"
-#include "genbank.h"
-#include "obfuscate.h"
+#include <crypto++/cryptlib.h>
+#include <crypto++/rijndael.h>
+#include <crypto++/modes.h>
+#include <crypto++/files.h>
+#include <crypto++/osrng.h>
+#include <crypto++/hex.h>
 
-/** TODO: Validate GenBank DNA sequence file format unstegging */
+#include "base64.h"
+#include "bpnn.h"
+#include "util.h"
+#include "types.h"
+#include "compression.h"
+
 
 using namespace std;
+using namespace CryptoPP;
+
 namespace po = boost::program_options;
 
-enum { SUCCESS, ERROR_IN_COMMAND_LINE, ERROR_UNHANDLED_EXCEPTION, INVALID_GENBANK_FILE };
+enum { SUCCESS, ERROR_IN_COMMAND_LINE, ERROR_UNHANDLED_EXCEPTION };
 
 static void help(const po::options_description& desc)
 {
-    string msg = "dnahide - hide messages in DNA"_hidden;
+    string msg = "mlsteg - hide messages in neural network weights";
     cout << msg << endl << desc << endl;
 }
 
-static void encrypt(vector<u8>& data, const string& password, const string& aad = "")
-{
-    string msg = "[*] Encrypting data..."_hidden;
-    cerr << msg << endl;
-
-    if (aad != "") {
-        vector<u8> aad_bytes(aad.begin(), aad.end());
-        vector<u8> kgk(32);
-        fastpbkdf2_hmac_sha256((u8*) password.data(), password.size(), (u8*) password.data(), password.size(),
-                               15000, kgk.data(), kgk.size());
-        AEAD<WordSize::BLOCK_128> aead(kgk);
-        aead.seal(data, aad_bytes, true);
-    } else {
-        RC6<WordSize::BLOCK_128> cipher{};
-        ECB<RC6<WordSize::BLOCK_128>> ecb(cipher);
-        CTR<ECB<RC6<WordSize::BLOCK_128>>> ctr(ecb, block_byte_size<WordSize::BLOCK_128>());
-        vector<u8> counter(16);
-        vector<u8> kgk(32);
-        fastpbkdf2_hmac_sha256((u8*) password.data(), password.size(), (u8*) password.data(), password.size(),
-                               15000, kgk.data(), kgk.size());
-        size_t padding = pad_to_block_size(data, block_byte_size<WordSize::BLOCK_128>());
-        ctr.crypt_parallel(data, kgk, counter);
-        // Snip padding length
-        data.erase(data.end() - padding, data.end());
-        // Append tag
-        data.insert(data.end(), counter.begin(), counter.end());
-    }
-}
-
-static void decrypt(vector<u8>& data, const string& password, const string& aad = "")
-{
-    string msg = "[*] Decrypting data..."_hidden;
-    cerr << msg << endl;
-
-    if (aad != "") {
-        vector<u8> aad_bytes(aad.begin(), aad.end());
-        vector<u8> kgk(32);
-        fastpbkdf2_hmac_sha256((u8*) password.data(), password.size(), (u8*) password.data(), password.size(),
-                               15000, kgk.data(), kgk.size());
-        // Create AEAD using RC6
-        AEAD<WordSize::BLOCK_128> aead(kgk);
-        aead.open(data, aad_bytes, true);
-    } else {
-        vector<u8> tag(data.end() - block_byte_size<WordSize::BLOCK_128>(), data.end());
-        RC6<WordSize::BLOCK_128> cipher{};
-        ECB<RC6<WordSize::BLOCK_128>> ecb(cipher);
-        CTR<ECB<RC6<WordSize::BLOCK_128>>> ctr(ecb, block_byte_size<WordSize::BLOCK_128>());
-        vector<u8> counter(16);
-        vector<u8> kgk(32);
-        fastpbkdf2_hmac_sha256((u8*) password.data(), password.size(), (u8*) password.data(), password.size(),
-                               15000, kgk.data(), kgk.size());
-        pad_to_block_size(data, block_byte_size<WordSize::BLOCK_128>());
-        ctr.crypt_parallel(data, kgk, counter);
-    }
-}
-
-static void steg_data(const string& password, const string& aad, const string& input_file,
+static void steg_data(const string& password, const string& input_file,
                       const string& output_file, bool disable_compression)
 {
     string data;
     stringstream ss;
-    vector<u8> compressed;
-    vector<u8> encrypted;
+    string compressed;
+    string encrypted;
     string dna;
 
     if (input_file != "") {
         if (file_exists(input_file)) {
-            string pre = "[*] File size: "_hidden;
-            string post = " bytes"_hidden;
+            string pre = "[*] File size: ";
+            string post = " bytes";
             cerr << pre << file_size(input_file.c_str()) << post << endl;
             data = read_file(input_file);
         } else {
-            string pre = "ERROR: File '"_hidden;
-            string post = "' does not exist.\n"_hidden;
+            string pre = "ERROR: File '";
+            string post = "' does not exist.\n";
             cerr << pre << input_file << post;
             exit(ERROR_IN_COMMAND_LINE);
         }
     } else {
-        string header = "<<< BEGIN STEGGED MESSAGE (Press CTRL+D when done) >>>\n\n"_hidden;
-        string footer = "<<< END STEGGED MESSAGE >>>\n\n"_hidden;
+        string header = "<<< BEGIN STEGGED NETWORK JSON (Press CTRL+D when done) >>>\n\n";
+        string footer = "<<< END STEGGED NETWORK JSON >>>\n\n";
         cerr << header;
         for (string line; getline(cin, line);)
             ss << line << endl;
@@ -122,25 +70,71 @@ static void steg_data(const string& password, const string& aad, const string& i
     if (data == "")
         data = ss.str();
 
-    vector<u8> input_data(data.begin(), data.end());
+    string input_data(data.begin(), data.end());
 
     if (!disable_compression) {
-        string compressing = "[*] Compressing..."_hidden;
+        string compressing = "[*] Compressing...";
         cerr << compressing << endl;
-        lzma::compress(data, compressed);
+        lzma::compress(input_data, compressed);
     }
 
     if (password != "") {
-        if (aad != "")
-            encrypt(encrypted = disable_compression ? input_data : compressed, password, aad);
-        else
-            encrypt(encrypted = disable_compression ? input_data : compressed, password);
+            AutoSeededRandomPool prng;
+            HexEncoder encoder(new FileSink(cout));
+
+            SecByteBlock key(AES::DEFAULT_KEYLENGTH);
+            SecByteBlock iv(AES::BLOCKSIZE);
+
+            prng.GenerateBlock(key, key.size());
+            prng.GenerateBlock(iv, iv.size());
+            string cipher, recovered;
+
+            cout << "plain text: " << (disable_compression ? input_data : compressed) << endl;
+
+            /*********************************\
+            \*********************************/
+
+            try {
+                CBC_Mode< AES >::Encryption e;
+                e.SetKeyWithIV(key, key.size(), iv);
+
+                StringSource s(disable_compression == true ? input_data : compressed, true, 
+                    new StreamTransformationFilter(e,
+                        new StringSink(cipher)
+                    ) // StreamTransformationFilter
+                ); // StringSource
+            } catch(const Exception& e) {
+                cerr << e.what() << endl;
+                exit(1);
+            }
+            
+            cout << "key: ";
+            encoder.Put(key, key.size());
+            encoder.MessageEnd();
+            cout << endl;
+
+            cout << "iv: ";
+            encoder.Put(iv, iv.size());
+            encoder.MessageEnd();
+            cout << endl;
+
+            cout << "cipher text: ";
+            encoder.Put((const byte*)&cipher[0], cipher.size());
+            encoder.MessageEnd();
+            cout << endl;
+        //encrypt(encrypted = disable_compression ? input_data : compressed, password);
     }
 
-    string encoding = "[*] Encoding DNA..."_hidden;
+    string encoding = "[*] Encoding network...";
+    b64 base64;
     cerr << encoding << endl;
-    dna = dna64::encode(password != "" ? encrypted : (disable_compression ? input_data : compressed));
-    dna = create_genbank_flatfile(dna);
+    // Map b64 characters to floats
+    // base64 encode message
+    // create expected data
+    // create magic inputs
+    // train on magic inputs to expected data
+    // dump network to JSON file
+    
 
     if (output_file != "") {
         ofstream ofs(output_file);
@@ -151,7 +145,7 @@ static void steg_data(const string& password, const string& aad, const string& i
     }
 }
 
-static void unsteg_data(const string& password, const string& aad, const string& input_file,
+static void unsteg_data(const string& password, const string& input_file,
                         const string& output_file, bool disable_compression)
 {
     string data = "";
@@ -160,41 +154,43 @@ static void unsteg_data(const string& password, const string& aad, const string&
         if (file_exists(input_file)) {
             data = read_file(input_file);
         } else {
-            string pre = "ERROR: File '"_hidden;
-            string post = "' does not exist.\n"_hidden;
+            string pre = "ERROR: File '";
+            string post = "' does not exist.\n";
             cerr << pre << input_file << post;
             exit(ERROR_IN_COMMAND_LINE);
         }
     } else {
-        string header = "<<< BEGIN DNA SEQUENCE MESSAGE (Press CTRL+D when done) >>>\n\n"_hidden;
-        string footer = "<<< END DNA SEQUENCE MESSAGE >>>\n\n"_hidden;
+        string header = "<<< BEGIN NETWORK JSON (Press CTRL+D when done) >>>\n\n";
+        string footer = "<<< END NETWORK JSON >>>\n\n";
         cout << header;
         for (string line; getline(cin, line);)
             data += line + "\n";
         cout << endl << footer;
     }
 
-    string decoding = "[*] Decoding DNA..."_hidden;
+    string decoding = "[*] Decoding network JSON...";
     cerr << decoding << endl;
-    string dna = parse_dna(data);
-    if (dna.size() == 0)
-        exit(INVALID_GENBANK_FILE);
-    string decoded = dna64::decode(dna);
+    // parse json
+    // build network
+    // feed inputs through network
+    // map output to characters
+    
+    // base64 decode
+    // decrypt
+    // decompress
+    string decoded;//= dna64::decode(dna);
     vector<u8> decrypted(decoded.begin(), decoded.end());
 
     if (password != "") {
-        if (aad != "")
-            decrypt(decrypted, password, aad);
-        else
-            decrypt(decrypted, password);
+        //decrypt(decrypted, password);
     }
 
     size_t decompressed_size = 0;
-    vector<u8> decompressed;
+    string decompressed;
     if (!disable_compression) {
-        string decompressing = "[*] Decompressing data..."_hidden;
+        string decompressing = "[*] Decompressing data...";
         cerr << decompressing << endl;
-        decompressed = vector<u8>(decrypted.size() * 4);
+        //decompressed = vector<u8>(decrypted.size() * 4);
         decompressed_size = lzma::decompress(decrypted, decompressed);
     }
 
@@ -206,8 +202,8 @@ static void unsteg_data(const string& password, const string& aad, const string&
             ofs.write(reinterpret_cast<const char*>(decrypted.data()), decrypted.size());
         ofs.close();
     } else {
-        string header = "<<< BEGIN RECOVERED MESSAGE >>>"_hidden;
-        string footer = "<<< END RECOVERED MESSAGE >>>"_hidden;
+        string header = "<<< BEGIN RECOVERED MESSAGE >>>";
+        string footer = "<<< END RECOVERED MESSAGE >>>";
         cerr << endl << header << endl << endl;
         if (!disable_compression)
             cout.write(reinterpret_cast<const char*>(decompressed.data()), decompressed_size);
@@ -219,23 +215,58 @@ static void unsteg_data(const string& password, const string& aad, const string&
 
 int main(int argc, char** argv)
 {
+    static default_random_engine gen;
+    static uniform_int_distribution<size_t> dis(0, 99);
+    b64 base64 = b64();
+    string alphabet = base64.idx();
+    random_shuffle(alphabet.begin(), alphabet.end());
+    map<char, float> mapping;
+    size_t i = 0;
+    for(char c : alphabet)
+        mapping[c] = i++;
+    string msg = "ohaithisisamessagewithalotofdifferentcharacterzandtransgressionsandthings";
+    string message = base64.encode(msg.c_str());
+    // encode
+    vector<float> expected;
+    for (char c : message)
+        expected.push_back(mapping[c] / 100);
+    vector<size_t> shape = {16, 10, 24, message.length()};
+    bpnn<float> nn(shape);
+    static uniform_real_distribution<float> dis2(0, 1);
+    vector<float> inputs(shape[0], 0);
+    vector<vector<float>> samples = { inputs };
+    vector<vector<float>> sample_expected  = { expected };
+    nn.train(samples, sample_expected, 10000);
+    vector<float> outputs = nn.forward(inputs);
+    string decoded;
+    for (float output: outputs) {
+        int round = output * 100 + .5;
+        for (auto& it : mapping) {
+            if (it.second == round) {
+                decoded += it.first;
+            }
+        }
+    }
+    cout << base64.decode(decoded);
+    
+    
+    
+    
     bool unsteg = "";
     string password = "";
     string output_file = "";
     string input_file = "";
-    string aad = "";
     bool disable_compression = false;
 
     try {
-        string options = "dnahide options"_hidden;
-        string help_switches = "help,h"_hidden, help_message = "print usage"_hidden;
-        string unsteg_switches = "unsteg,u"_hidden, unsteg_message = "unsteg message"_hidden;
-        string input_switches = "input,i"_hidden, input_message = "input file"_hidden;
-        string output_switches = "output,o"_hidden, output_message = "output file"_hidden;
-        string pass_switches = "password,p"_hidden, pass_message = "encryption password"_hidden;
-        string aad_switches = "aad,a"_hidden, aad_message = "additional authenticated data"_hidden;
-        string disable_compression_switches = "disable-compression"_hidden,
-               disable_compression_message = "disable compression"_hidden;
+        string options = "mlsteg options";
+        string help_switches = "help,h", help_message = "print usage";
+        string unsteg_switches = "unsteg,u", unsteg_message = "unsteg message";
+        string input_switches = "input,i", input_message = "input file";
+        string output_switches = "output,o", output_message = "output file";
+        string pass_switches = "password,p", pass_message = "encryption password";
+        string disable_compression_switches = "disable-compression",
+               disable_compression_message = "disable compression";
 
         po::options_description desc(options);
         // clang-format off
@@ -244,7 +275,6 @@ int main(int argc, char** argv)
             unsteg_message.c_str())(input_switches.c_str(), po::value(&input_file), input_message.c_str())(
             output_switches.c_str(), po::value(&output_file), output_message.c_str())(
             pass_switches.c_str(), po::value(&password), pass_message.c_str())(
-            aad_switches.c_str(), po::value(&aad), aad_message.c_str())(
             disable_compression_switches.c_str(), po::bool_switch(&disable_compression), disable_compression_message.c_str());
         // clang-format on
 
@@ -261,18 +291,18 @@ int main(int argc, char** argv)
             po::notify(vm);
 
             if (unsteg)
-                unsteg_data(password, aad, input_file, output_file, disable_compression);
+                unsteg_data(password, input_file, output_file, disable_compression);
             else
-                steg_data(password, aad, input_file, output_file, disable_compression);
+                steg_data(password, input_file, output_file, disable_compression);
         } catch (po::error& e) {
-            string pre = "ERROR: "_hidden;
+            string pre = "ERROR: ";
             cerr << pre << e.what() << endl << endl;
             cerr << desc << endl;
             return ERROR_IN_COMMAND_LINE;
         }
     } catch (exception& e) {
-        string pre = "ERROR: unhandled exception reached the top of main: "_hidden;
-        string post = ", application will now exit"_hidden;
+        string pre = "ERROR: unhandled exception reached the top of main: ";
+        string post = ", application will now exit";
         cerr << pre << e.what() << post << endl;
         return ERROR_UNHANDLED_EXCEPTION;
     }
